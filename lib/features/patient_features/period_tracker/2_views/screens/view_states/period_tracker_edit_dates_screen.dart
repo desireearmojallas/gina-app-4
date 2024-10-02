648af61dc33b202cdc8cfcd892359488a169b5d1
Desiree Armojallas <desiree.armojallas.20@usjr.edu.ph>
2024-10-03 01:20:16 +0800

    Import changes to Edit Dates Screen. Duplicate import

--- lib/features/patient_features/period_tracker/2_views/screens/view_states/period_tracker_edit_dates_screen.dart
+++ lib/features/patient_features/period_tracker/2_views/screens/view_states/period_tracker_edit_dates_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:gina_app_4/core/theme/theme_service.dart';
 import 'package:gina_app_4/features/patient_features/period_tracker/2_views/screens/widgets/period_tracker_legend.dart';
-import 'package:gina_app_4/features/patient_features/period_tracker/2_views/screens/widgets/yearly_calendar_widget.dart';
+import 'package:gina_app_4/features/patient_features/period_tracker/2_views/screens/widgets/period_tracker_widgets/yearly_calendar_widget.dart';
 
 class PeriodTrackerEditDatesScreen extends StatelessWidget {
   const PeriodTrackerEditDatesScreen({super.key});
