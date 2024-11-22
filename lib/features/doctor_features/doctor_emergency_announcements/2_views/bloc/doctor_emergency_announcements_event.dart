1a480aa78e5785d75c8a3afe174bd19004f0e150
Desiree Armojallas <desiree.armojallas.20@usjr.edu.ph>
2024-11-22 18:10:17 +0800

    Doctor Emergency Announcements Initial

---
+++ lib/features/doctor_features/doctor_emergency_announcements/2_views/bloc/doctor_emergency_announcements_event.dart
@@ -0,0 +1,8 @@
+part of 'doctor_emergency_announcements_bloc.dart';
+
+sealed class DoctorEmergencyAnnouncementsEvent extends Equatable {
+  const DoctorEmergencyAnnouncementsEvent();
+
+  @override
+  List<Object> get props => [];
+}
