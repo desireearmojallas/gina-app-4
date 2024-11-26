d06a5c1c5dae72194b1851b9a117567c316857c4
Desiree Armojallas <desiree.armojallas.20@usjr.edu.ph>
2024-11-26 14:27:30 +0800

    Admin Feature - Doctor Verification Function Initial

--- lib/features/admin_features/admin_doctor_verification/1_controllers/admin_doctor_verification_controller.dart
+++ lib/features/admin_features/admin_doctor_verification/1_controllers/admin_doctor_verification_controller.dart
@@ -1,5 +1,13 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:gina_app_4/features/auth/0_model/doctor_model.dart';
+
 class AdminDoctorVerificationController {
-  final FirebaseAuth auth = FirebaseAuth.insance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   late StreamSubscription authStream;
   FirebaseAuthException? error;
@@ -7,5 +15,32 @@ class AdminDoctorVerificationController {
 
   //--------------- GET ALL DOCTORS -------------------
 
-  Future<Either<Exception, List<DoctorModel>>> getAllDoctors() async {}
+  Future<Either<Exception, List<DoctorModel>>> getAllDoctors() async {
+    try {
+      final doctorSnapshot = await firestore.collection('doctors').get();
+
+      if (doctorSnapshot.docs.isNotEmpty) {
+        final doctorList = doctorSnapshot.docs
+            .map((doctor) => DoctorModel.fromJson(doctor.data()))
+            .toList();
+
+        return Right(doctorList);
+      }
+
+      return const Right([]);
+    } on FirebaseAuthException catch (e) {
+      debugPrint(e.message);
+      debugPrint(e.code);
+      working = false;
+      error = e;
+      return Left(
+        Exception(e.message),
+      );
+    } catch (e) {
+      debugPrint(e.toString());
+      working = false;
+      error = FirebaseAuthException(message: e.toString(), code: 'error');
+      return Left(Exception(e.toString()));
+    }
+  }
 }
